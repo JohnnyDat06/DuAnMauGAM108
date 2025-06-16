8ce046563ebd40fe040311e62dd080ca4113b944
JohnnyDat06 <hotandat4321@gmail.com>
2025-06-16 13:50:41 +0700

    Int

---
+++ Assets/Script/Enemy/EnemyManage.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManage : MonoBehaviour
+{
+
+    [SerializeField] private Transform checkPoint1;
+    [SerializeField] private Transform checkPoint2;
+    [SerializeField] private Rigidbody2D platformRigidbody;
+    [SerializeField] private float moveSpeed = 3f;
+
+    private Transform targetPoint;
+
+    void Start()
+    {
+        if (checkPoint1 != null && checkPoint2 != null)
+        {
+            transform.position = checkPoint1.position;
+            targetPoint = checkPoint2;
+        }
+    }
+
+    void Update()
+    {
+        if (checkPoint1 == null || checkPoint2 == null) return;
+        Vector2 newPosition = Vector2.MoveTowards(platformRigidbody.position, targetPoint.position,
+            moveSpeed * Time.fixedDeltaTime);
+        platformRigidbody.MovePosition(newPosition);
+        if (Vector2.Distance(transform.position, targetPoint.position) < 0.1f)
+        {
+            if (targetPoint == checkPoint1)
+            {
+                targetPoint = checkPoint2;
+            }
+            else
+            {
+                targetPoint = checkPoint1;
+            }
+        }
+        if (checkPoint1 == null || checkPoint2 == null) return;          
+
+            // Flip theo hướng di chuyển
+            Vector2 direction = targetPoint.position - transform.position;
+            if (direction.x > 0.01f)
+            {
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+            else if (direction.x < -0.01f)
+            {
+                transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+
+            if (Vector2.Distance(transform.position, targetPoint.position) < 0.1f)
+            {
+                targetPoint = (targetPoint == checkPoint1) ? checkPoint2 : checkPoint1;
+            }
+
+    }
+}
---
+++ Assets/Script/Player/PlayerMovent.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField] private Rigidbody2D playerRigidbody;
+	[SerializeField] private float moveSpeed = 6f;
+	[SerializeField] private float jumpForce = 10f;
+	[SerializeField] private Animator playerAnimator;
+	[SerializeField] private SpriteRenderer playerSpriteRenderer;
+	[SerializeField] private BoxCollider2D playerCollider;
+	[SerializeField] private LayerMask terrainLayer;
+
+	private bool jumpCheck = false;
+	private GameObject currentPlatform = null;
+
+	void Update()
+	{
+		Movement();
+		UpdateAnimator();
+	}
+
+	private void Movement()
+	{
+		float horizontal = Input.GetAxis("Horizontal");
+		playerRigidbody.velocity = new Vector2(horizontal * moveSpeed, playerRigidbody.velocity.y);
+		if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+		{
+			playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, jumpForce);
+		}
+		if (IsGrounded() == true)
+		{
+			jumpCheck = true;
+		}
+		if (IsGrounded() == false && jumpCheck == true && Input.GetKeyDown(KeyCode.Space))
+		{
+			playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, jumpForce);
+			jumpCheck = false;
+		}
+
+		//Bấm S khi đứng trên platform sẽ rơi xuống
+		if (Input.GetKeyDown(KeyCode.S))
+		{
+			if (currentPlatform != null)
+			{
+				PlatformController platformController = currentPlatform.GetComponent<PlatformController>();
+				if (platformController != null)
+				{
+					platformController.DropPlayer();
+					playerRigidbody.AddForce(Vector2.down * 2f, ForceMode2D.Impulse);
+				}
+			}
+		}
+	}
+
+	private void UpdateAnimator()
+	{
+		if (playerRigidbody.velocity.x < 0)
+		{
+			playerSpriteRenderer.flipX = true;
+		}
+		else if (playerRigidbody.velocity.x > 0)
+		{
+			playerSpriteRenderer.flipX = false;
+		}
+
+		if (Input.anyKey == true)
+		{
+			playerAnimator.SetBool("IsMove", true);
+		}
+		else
+		{
+			playerAnimator.SetBool("IsMove", false);
+		}
+		if (playerRigidbody.velocity.y > .1f)
+		{
+			playerAnimator.SetInteger("State", 1);
+		}
+		else if (playerRigidbody.velocity.y < -.1f)
+		{
+			playerAnimator.SetInteger("State", -1);
+		}
+		else
+		{
+			playerAnimator.SetInteger("State", 0);
+		}
+	}
+
+	private bool IsGrounded()
+	{
+		return Physics2D.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size, 0f,
+			Vector2.down, 0.1f, terrainLayer);
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Platform"))
+		{
+			ContactPoint2D[] contacts = new ContactPoint2D[1];
+			collision.GetContacts(contacts);
+			if (contacts[0].normal.y > 0.5f)
+			{
+				currentPlatform = collision.gameObject;
+			}
+		}
+	}
+
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		if (collision.gameObject == currentPlatform)
+		{
+			currentPlatform = null;
+		}
+	}
+}
